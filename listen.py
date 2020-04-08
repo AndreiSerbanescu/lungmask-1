31b7981bcd85d2df7eb0fd440c1cd1c970fb36bc
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-04-08 21:56:39 +0300

    implement calling converter command

--- listen.py
+++ listen.py
@@ -6,6 +6,7 @@ from urllib.parse import parse_qs
 import logging
 import sys
 import json
+import time
 
 class CommandRequestHandler(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -61,10 +62,19 @@ def mark_yourself_ready():
 
 
 def convert(param_dict):
-    # TODO implement
-    print("### converter got parameters".format(param_dict))
+    print("### converter got parameters {}".format(param_dict))
 
-    result_dict = {}
+    rel_source_dir = param_dict["source_dir"][0]
+    data_share     = os.environ["DATA_SHARE_PATH"]
+    source_dir     = os.path.join(data_share, rel_source_dir)
+
+    rel_output_dir = "converter-output-" + str(time.time()) + ".nii.gz"
+    output_dir     = os.path.join(data_share, rel_output_dir)
+
+    conversion_command = "python3 /app/convert.py {} && mv {}.nii.gz {}".format(source_dir, source_dir, output_dir)
+    sb.call([conversion_command], shell=True)
+
+    result_dict = { "filename":  rel_output_dir}
     return result_dict
 
 
